309025591eff2886a2567bf74c45b0755c8ac022
Wesley Alves <wesandradealves@gmail.com>
2018-08-28 03:49:50 -0300

    - Cart exceto cadastro, pagamento e conclusao.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -49,15 +49,49 @@ function checkBlur(){
 function modalClose(e){
     var e = $(e);
     e.closest(".modal").removeClass("-toggle");
+    e.closest(".modal").find("h3").html("");
     e.closest(".modal").find("#modal-inner-content").html("");
     checkBlur();
 }
-function modalOpen(e, modal){
+function modalOpen(e, post){
     var e = $(e),
         html,
+        titulo,
         modal = $(".modal");
-    switch(modal){
-        case modal :
+
+        $.getJSON('estados_cidades.json', function (data) {
+            var items = [];
+            var options = '<option value="">Escolha um estado</option>';    
+            $.each(data, function (key, val) {
+                options += '<option value="' + val.nome + '">' + val.nome + '</option>';
+            });                 
+            $(".estados").html(options);                
+            
+            $(".estados").change(function () {              
+            
+                var options_cidades = '';
+                var str = "";                   
+                
+                $(".estados option:selected").each(function () {
+                    str += $(this).text();
+                });
+                
+                $.each(data, function (key, val) {
+                    if(val.nome == str) {                           
+                        $.each(val.cidades, function (key_city, val_city) {
+                            options_cidades += '<option value="' + val_city + '">' + val_city + '</option>';
+                        });                         
+                    }
+                });
+                $(this).closest("form").find(".cidades").html(options_cidades);
+                
+            }).change();        
+        
+        });
+
+    switch(post){
+        case 'login' :
+            titulo = 'Acesse sua conta';
             html = '<form action="acompanhe-seu-pedido.html" class="forms -modal">';
               html += '<div class="column">';
                 html += '<span>';
@@ -71,10 +105,74 @@ function modalOpen(e, modal){
                 html += '</span>';
               html += '</div>';
             html += '</form>';
-            modal.find(".modal-inner-content").html(html)
+        break;
+        case 'endereco' :
+            titulo = 'Alterar endereço';
+            html = '<form action="" class="forms -modal">';
+              html += '<div class="column">';
+                html += '<span>';
+                html += '<label for="">CEP</label>';
+                html += '<input class="cep" type="text" value="95020-450" />';
+                html += '</span>';
+                html += '<span>';
+                html += '<label for="">Endereço</label>';
+                html += '<input class="rua" type="text" value="RUA VINTE DE SETEMBRO">';
+                html += '</span>';
+                html += '<span class="numero-complemento">';
+                html += '<span>';
+                    html += '<label for="">Número</label>';
+                    html += '<input type="number" value="3020" maxlength="6" />';
+                html += '</span>';
+                html += '<span>';
+                    html += '<label for="">Complemento</label>';
+                    html += '<input type="text" value="APTO, 54" />';
+                html += '</span>';
+                html += '</span>';
+                html += '<span>';
+                html += '  <label for="">Bairro</label>';
+                html += '  <input class="bairro" type="text" value="Nossa Senhora de Lourdes" />';
+                html += '</span>';
+                html += '<span>';
+                html += '  <label for="">Cidade</label>';
+                html += '<span class="custom-select">';
+                html += '    <select class="cidades" name="">';
+                html += '    </select>';
+                html += '</span> ';
+                html += '</span>';
+                html += '<span class="numero-complemento">';
+                html += '  <span>';
+                html += '    <label for="">UF</label>';
+                html += '    <input onkeypress="mascara(this,soLetras)" class="uf" type="text" value="RS" maxlength="2" />';
+                html += '  </span>';
+                html += '  <span>';
+                html += '    <label for="">Estado</label>';
+                html += '    <span class="custom-select">';
+                html += '        <select class="estados" name="">';
+                html += '        </select>';
+                html += '    </span> '; 
+                html += '  </span>';
+                html += '</span>';
+                html += '<span class="numero-complemento -telefone">';
+                html += '  <span>';
+                html += '    <label for="">Telefone</label>';
+                html += '    <input class="telefone" type="text" value="(54) 99999999" />';
+                html += '  </span>';
+                html += '  <span>';
+                html += '      <label for="">Celular</label>';
+                html += '      <input class="celular" type="text" value="(54) 999999999" />';
+                html += '  </span>';
+                html += '</span>';
+                html += '<span>';
+                html += '    <button class="btn btn-1 -action"><span>Alterar este endereço</span> <i class="fa fa-angle-right"></i></button>';
+                html += '</span>';
+              html += '</div>';
+            html += '</form>';
         break;
         default :
     }
+    modal.find(".modal-inner-content").html(html),
+    modal.find("h3").html(titulo);
+
     $(".modal").addClass("-toggle");
     checkBlur();
 }
@@ -119,63 +217,63 @@ $.getJSON('estados_cidades.json', function (data) {
 
 });
 // Autocomplete CEP
-// function limpa_formulário_cep() {
-//     $(".cep").val("");
-//     $(".rua").val("");
-//     $(".bairro").val("");
-//     $(".cidade").val("");
-//     $(".uf").val("");
-// }   
-// $( ".cep" ).each(function() {
-//     $(this).blur(function() {
-//         //Nova variável "cep" somente com dígitos.
-//         var cep = $(this).val().replace(/\D/g, '');
+function limpa_formulário_cep() {
+    $(".cep").val("");
+    $(".rua").val("");
+    $(".bairro").val("");
+    $(".cidade").val("");
+    $(".uf").val("");
+}   
+$( ".cep" ).each(function() {
+    $(this).blur(function() {
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
 
-//         //Verifica se campo cep possui valor informado.
-//         if ($(this).val() != "") {
+        //Verifica se campo cep possui valor informado.
+        if ($(this).val() != "") {
 
-//             //Expressão regular para validar o CEP.
-//             var validacep = /^[0-9]{8}$/;
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
 
-//             //Valida o formato do CEP.
-//             if(validacep.test(cep)) {
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
 
-//                 //Preenche os campos com "..." enquanto consulta webservice.
-//                 $(this).closest($("form")).find(".rua").val("...");
-//                 $(this).closest($("form")).find(".bairro").val("...");
-//                 $(this).closest($("form")).find(".cidade").val("...");
-//                 $(this).closest($("form")).find(".uf").val("...");
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $(this).closest($("form")).find(".rua").val("...");
+                $(this).closest($("form")).find(".bairro").val("...");
+                $(this).closest($("form")).find(".cidade").val("...");
+                $(this).closest($("form")).find(".uf").val("...");
 
-//                 //Consulta o webservice viacep.com.br/
-//                 $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
 
-//                     if (!("erro" in dados)) {
-//                         //Atualiza os campos com os valores da consulta.
-//                         $(this).closest($("form")).find(".rua").val(dados.logradouro);
-//                         $(this).closest($("form")).find(".bairro").val(dados.bairro);
-//                         $(this).closest($("form")).find(".cidade").val(dados.localidade);
-//                         $(this).closest($("form")).find(".uf").val(dados.uf);
-//                         console.log(dados);
-//                     } //end if.
-//                     else {
-//                         //CEP pesquisado não foi encontrado.
-//                         limpa_formulário_cep();
-//                         alert("CEP não encontrado.");
-//                     }
-//                 });
-//             } //end if.
-//             else {
-//                 //cep é inválido.
-//                 limpa_formulário_cep();
-//                 alert("Formato de CEP inválido.");
-//             }
-//         } //end if.
-//         else {
-//             //cep sem valor, limpa formulário.
-//             limpa_formulário_cep();
-//         }
-//     });   
-// });
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $(this).closest($("form")).find(".rua").val(dados.logradouro);
+                        $(this).closest($("form")).find(".bairro").val(dados.bairro);
+                        $(this).closest($("form")).find(".cidade").val(dados.localidade);
+                        $(this).closest($("form")).find(".uf").val(dados.uf);
+                        console.log(dados);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });   
+});
 function rating(){
     $(".rating-stars").find("input").each(function ( index ) {
         var input = $(this);
